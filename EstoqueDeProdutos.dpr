a796a04800d09f717052a2bfa00a282b1d6d5c48
Fábio Júnior Barbosa <fjb.desenv@gmail.com>
2024-07-14 05:26:55 -0300

    Adicionado icone

--- EstoqueDeProdutos.dpr
+++ EstoqueDeProdutos.dpr
@@ -4,7 +4,9 @@ uses
   Vcl.Forms,
   UnitLogin in 'Forms\UnitLogin.pas' {FormLogin},
   UnitDMLogin in 'DMs\UnitDMLogin.pas' {DMLogin: TDataModule},
-  UnitPrincipal in 'Forms\UnitPrincipal.pas' {FormPrincipal};
+  UnitPrincipal in 'Forms\UnitPrincipal.pas' {FormPrincipal},
+  Usuario in 'Class\Usuario.pas',
+  UnitSobre in 'Forms\UnitSobre.pas' {FormSobre};
 
 {$R *.res}
 
@@ -14,5 +16,6 @@ begin
   Application.CreateForm(TFormLogin, FormLogin);
   Application.CreateForm(TDMLogin, DMLogin);
   Application.CreateForm(TFormPrincipal, FormPrincipal);
+  Application.CreateForm(TFormSobre, FormSobre);
   Application.Run;
 end.
